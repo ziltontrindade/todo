cf5e52435b156e6ecfc87706eae4a0bda9c83e1e
Zilton Cordeiro <ztc941@gmail.com>
2022-09-29 13:48:30 -0300

    Criada as Migrations

---
+++ database/migrations/2022_09_29_162132_create_categories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->string('color')->default('#FFF');
+            $table->foreignIdFor(User::class)->references('id')->on('users')->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {   
+        Schema::table('categories', function(Blueprint $table){
+            $table->dropForeignIdFor(User::class);
+        });
+
+        Schema::dropIfExists('categories');
+    }
+};
